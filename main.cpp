6e9c6b1259e89f46b5875d6b6bcf06ffe2518e58
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2015-05-16 15:18:15 +0300

    Up

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+
     return 0;
+}
+
+
+
+int func(){
+    double dval = 3.4;   /// Ýkisindede geçerli
+
+
+    int ival {dval}; /// Bu syntax de tür dönüþümü syntax hatasý deðer kaybý engellenebilir.  Narrowing conversion  ilk deðer verme syntaxi
+
+
+
 }
