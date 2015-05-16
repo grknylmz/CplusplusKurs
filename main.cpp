eea0a5fd5ff330ab2bba03a3e99737200621f161
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2015-05-16 16:59:42 +0300

    Up

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ int func(){
     double dval = 3.4;   /// Ýkisindede geçerli
 
 
-    int ival {dval}; /// Bu syntax de tür dönüþümü syntax hatasý deðer kaybý engellenebilir.  Narrowing conversion  ilk deðer verme syntaxi
+    int ival {dval}; /// Bu syntax de tür dönüþümü syntax hatasý deðer kaybý engellenebilir.
+    // Narrowing conversion  ilk deðer verme syntaxi
 
 
 
